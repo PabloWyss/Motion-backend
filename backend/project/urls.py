d1f5ebbf4c52c6829595e5212fd92633d207a223
Pablo Wyss <pabloawyss@gmail.com>
2023-03-27 17:56:06 +0200

    registration profile created. Missing validation prosess pass lint

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
-from rest_framework_simplejwt import views as jwt_views
 
 schema_view = get_schema_view(
     openapi.Info(
--- backend/registrationprofile/models.py
+++ backend/registrationprofile/models.py
@@ -3,8 +3,6 @@ import random
 
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 User = get_user_model()
 
