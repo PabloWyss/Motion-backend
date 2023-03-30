dbeb0480b3214fcceb0dbab6acd4758cd5f00778
Nicolas Ponce <nicolas.ponce@vonous.com>
2023-03-30 09:12:30 +0200

    update settings.py

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -47,10 +47,11 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
-
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
