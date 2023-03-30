1123f54d4f5b66fdd3213436ae30581a59157fb8
Pablo Wyss <pabloawyss@gmail.com>
2023-03-30 12:24:46 +0200

    pass lint

--- backend/post_image/serializers.py
+++ backend/post_image/serializers.py
@@ -4,7 +4,7 @@ from post_image.models import Attachment
 
 class PostImageSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(max_length=None, use_url=True)
+
     class Meta:
         model = Attachment
         fields = '__all__'
-
